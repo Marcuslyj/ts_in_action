633f8cdf804f8ffc64b56b157d2bed27bddf6bfd
Apple <814712074@qq.com>
2019-08-29 22:07:56 +0800

    eslint检查

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ class A {
     a: number = 1
 }
 
-let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 }
-let n = { x, y, ...z }
+const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 }
+const n = { x, y, ...z }
 
 // n = 1
 
-let str: string = "hello"
+const str: string = "hello"
 document.write(str)
