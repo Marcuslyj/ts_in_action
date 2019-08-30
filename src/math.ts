748580774659a0e97bc5e8d6e16316d494134675
liyongjie <liyongjie@tvflnet.com>
2019-08-30 10:56:16 +0800

    集成jest，单元测试（类型检测还是要启动类型检测脚本）

---
+++ src/math.ts
@@ -0,0 +1,14 @@
+export function add(a: number, b: number): number {
+    return a + b
+}
+
+export function sub(a: number, b: number): number {
+    return a - b
+}
+
+export default {
+    add,
+    sub
+}
+
+// const n: number = '1'
